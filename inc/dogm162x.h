39f26f1ee17d2b8c4a57856c90701f57d13d7dde
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2016-05-12 18:39:57 +0200

    removed avr dependencies and added gotoPos function

--- inc/dogm162x.h
+++ inc/dogm162x.h
@@ -4,14 +4,20 @@
 #include <stdint.h>
 
 #define EXT_SPI_TRANSFER dogm162x_spi
+#define EXT_DELAY_200_MS dogm_delay_ms
+#define EXT_DELAY_24_US  dogm_delay_us
 
 extern uint8_t EXT_SPI_TRANSFER(uint8_t c);
+extern void EXT_DELAY_200_MS(void);
+extern void EXT_DELAY_24_US(void);
 
 void dogm162x_init(void);
 
 void dogm162x_puts(char* s);
 
 void dogm162x_putsPos(uint8_t x, uint8_t y, char* s);
+
+void dogm162x_gotoPos(uint8_t x, uint8_t y);
 
 #endif //DOGM162X_H 
 
--- src/dogm162x.c
+++ src/dogm162x.c
@@ -1,61 +1,59 @@
 #include "../inc/dogm162x.h"
-#include <avr/io.h>
-#include <util/delay.h>
 
 #define CMD_WAIT_US  27
 #define CMD_WAIT_MS  200
 
-//DOGM126x COMMANDS
+//DOGM162x COMMANDS
 
-#define DOGM126_CMD_CLEAR_DISPLAY       0x01
-#define DOGM126_CMD_RETURN_HOME         0x02
+#define DOGM162_CMD_CLEAR_DISPLAY       0x01
+#define DOGM162_CMD_RETURN_HOME         0x02
 
-#define DOGM126_CMD_ENTRY_MODE          0x04
-#define DOGM126_ENTRY_SHIFT_UP          0x01
-#define DOGM126_ENTRY_SHIFT_DOWN        0x00
-#define DOGM126_ENTRY_MOVE_LEFT         0x00
-#define DOGM126_ENTRY_MOVE_RIGHT        0x02
+#define DOGM162_CMD_ENTRY_MODE          0x04
+#define DOGM162_ENTRY_SHIFT_UP          0x01
+#define DOGM162_ENTRY_SHIFT_DOWN        0x00
+#define DOGM162_ENTRY_MOVE_LEFT         0x00
+#define DOGM162_ENTRY_MOVE_RIGHT        0x02
 
-#define DOGM126_CMD_DIPLAY_ON_OFF       0x08
-#define DOGM126_ENTIRE_DISPLAY_ON       0x04
-#define DOGM126_ENTIRE_DISPLAY_OFF      0x00
-#define DOGM126_CURSOR_ON               0x02
-#define DOGM126_CURSOR_OFF              0x00
-#define DOGM126_CURSOR_POSITION_ON      0x01
-#define DOGM126_CURSOR_POSITION_OFF     0x00
+#define DOGM162_CMD_DIPLAY_ON_OFF       0x08
+#define DOGM162_ENTIRE_DISPLAY_ON       0x04
+#define DOGM162_ENTIRE_DISPLAY_OFF      0x00
+#define DOGM162_CURSOR_ON               0x02
+#define DOGM162_CURSOR_OFF              0x00
+#define DOGM162_CURSOR_POSITION_ON      0x01
+#define DOGM162_CURSOR_POSITION_OFF     0x00
 
-#define DOGM126_CMD_SET_DDRAM_ADDRESS   0x80
-#define DOGM126_DDRAM(x)                (x & 0x7F)
+#define DOGM162_CMD_SET_DDRAM_ADDRESS   0x80
+#define DOGM162_DDRAM(x)                (x & 0x7F)
 
 
 //Commands have to be completed with a RS line toggle!!!
 
 
-static const char init_sequence[] = {0x39, 0x1C, 0x52, 0x69, 0x74, 
+static const char init_sequence[] = {0x39, 0x1C, 0x52, 0x69, 0x71, 
     0x3C, 0x0F, 0x01, 0x06};
 
 
 void dogm162x_init(void)
 {
-    _delay_ms(CMD_WAIT_MS);
+    EXT_DELAY_200_MS();
     EXT_SPI_TRANSFER(init_sequence[0]);
-    _delay_us(CMD_WAIT_US);
+    EXT_DELAY_24_US();
     EXT_SPI_TRANSFER(init_sequence[1]);
-    _delay_us(CMD_WAIT_US);
+    EXT_DELAY_24_US();
     EXT_SPI_TRANSFER(init_sequence[2]);
-    _delay_us(CMD_WAIT_US);
+    EXT_DELAY_24_US();
     EXT_SPI_TRANSFER(init_sequence[3]);
-    _delay_us(CMD_WAIT_US);
+    EXT_DELAY_24_US();
     EXT_SPI_TRANSFER(init_sequence[4]);
-    _delay_us(CMD_WAIT_US);
+    EXT_DELAY_24_US();
     EXT_SPI_TRANSFER(init_sequence[5]);
-    _delay_us(CMD_WAIT_US);
+    EXT_DELAY_24_US();
     EXT_SPI_TRANSFER(init_sequence[6]);
-    _delay_us(CMD_WAIT_US);
+    EXT_DELAY_24_US();
     EXT_SPI_TRANSFER(init_sequence[7]);
-    _delay_ms(CMD_WAIT_MS);
+    EXT_DELAY_200_MS();
     EXT_SPI_TRANSFER(init_sequence[8]);
-    _delay_us(CMD_WAIT_US);
+    EXT_DELAY_24_US();
 
 }
 
@@ -67,6 +65,13 @@ void dogm162x_puts(char* s)
 void dogm162x_putsPos(uint8_t x, uint8_t y, char* s)
 {
 
+}
+
+void dogm162x_gotoPos(uint8_t x, uint8_t y)
+{
+    uint8_t cmd = DOGM162_CMD_SET_DDRAM_ADDRESS |
+		  ((y << 2) + x);
+    EXT_SPI_TRANSFER(cmd);
 }
 
 
